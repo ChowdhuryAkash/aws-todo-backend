4581acc2c1ea60536644580569519d7b34a48143
Akash Chowdhury <emailidakashchowdhury@gmail.com>
2025-07-29 23:05:12 +0530

    kgkb

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', tasksRoutes);
