ff799afbf19b78a5707c6e0dfced63ae53a3097c
Akash Chowdhury <emailidakashchowdhury@gmail.com>
2025-07-29 13:25:20 +0530

    dfdafd

--- server.js
+++ server.js
@@ -19,5 +19,9 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', tasksRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Task Management API');
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
